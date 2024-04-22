0df56217c44eaf05b31a1af02b7a43ae0170599d
Ángel Lucas González Martínez <agonzalez@fi.upm.es>
2024-04-22 13:57:00 +0200

    Excepciones

---
+++ T6Vaso/src/vajilla/exception/ErrorVasoLLeno.java
@@ -0,0 +1,59 @@
+/**
+ * 
+ */
+package vajilla.exception;
+
+/**
+ * 
+ */
+public class ErrorVasoLLeno extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 3123076322596809651L;
+
+	/**
+	 * 
+	 */
+	public ErrorVasoLLeno() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param message
+	 */
+	public ErrorVasoLLeno(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param cause
+	 */
+	public ErrorVasoLLeno(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param message
+	 * @param cause
+	 */
+	public ErrorVasoLLeno(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param message
+	 * @param cause
+	 * @param enableSuppression
+	 * @param writableStackTrace
+	 */
+	public ErrorVasoLLeno(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+}
