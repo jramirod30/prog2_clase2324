164cbb681320e650dc81bf1c8a9bdcc4541890bc
Ángel Lucas González Martínez <agonzalez@fi.upm.es>
2024-04-22 10:42:06 +0200

    Se añade el lector

--- T6Vaso/src/app/PruebaVaso.java
+++ T6Vaso/src/app/PruebaVaso.java
@@ -1,6 +1,7 @@
 package app;
 
 import vajilla.Vaso;
+import java.util.Scanner;
 
 public class PruebaVaso {
 
@@ -8,7 +9,7 @@ public class PruebaVaso {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		
+		//Scanner lector = new Scanner(System.in);
 		Vaso vaso = new Vaso(10.0);
 		vaso.llenar(5.0);
 		System.out.println(vaso);
@@ -17,6 +18,7 @@ public class PruebaVaso {
 		vaso.llenar(5.0);
 		System.out.println(vaso);
 		vaso.prepararDesayuno(10, 2, 1);
+		//lector.close();
 
 	}
 
